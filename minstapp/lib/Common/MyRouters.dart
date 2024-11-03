ec3d2185ce622110d35a5d0d66105221e900e493
AzureAD\MarkEdward <mespinozar@lucky.com.pe>
2024-11-02 21:56:12 -0500

    Login

--- minstapp/lib/Common/MyRouters.dart
+++ minstapp/lib/Common/MyRouters.dart
@@ -2,15 +2,20 @@
 
 import 'package:flutter/material.dart';
 import 'package:minstapp/pages/login/loginApp.dart';
+import 'package:minstapp/pages/menu/menu.dart';
 
 // ignore: constant_identifier_names
 const String ROUTE_LOGIN = '/login';
+// ignore: constant_identifier_names
+const String ROUTE_MENU = '/menu';
 
 class MyRouters {
   static Route<dynamic>generateRoute(RouteSettings settings){
     switch(settings.name){
       case "/login":
         return MaterialPageRoute(builder: (_)=>const LoginPage());
+      case "/menu":
+        return MaterialPageRoute(builder: (_)=>const MenuPage());
       default:
         return MaterialPageRoute(builder: (_)=>const LoginPage());
     }
--- minstapp/lib/pages/login/loginApp.dart
+++ minstapp/lib/pages/login/loginApp.dart
@@ -1,14 +1,147 @@
+// ignore_for_file: file_names
 import 'package:flutter/material.dart';
-
+import 'package:minstapp/Common/MyRouters.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Login Page'),
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: <Color>[
+              Colors.white,
+              Colors.white,
+            ],
+            )
       ),
-    );
+      child: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const Icon(
+                  Icons.library_add, 
+                  size: 150, 
+                  color: Colors.greenAccent,),
+                const Text("mInst",
+                  style: TextStyle(
+                    fontSize: 70,
+                    color: Colors.blueAccent,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text("Bienvenido",
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.blueAccent,
+                  ),
+                ),
+                const SizedBox(height: 30),
+                //TEXTBOX USUARIO
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    borderRadius: BorderRadius.circular(15),
+                    ),
+                  child: const Padding(
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  child:  TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Usuario",
+                      ),
+                    )
+                   )
+                  ),
+                ),
+                //
+                const SizedBox(height: 30),
+                //TEXTBOX CONTRASEÑA
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    borderRadius: BorderRadius.circular(15),
+                    ),
+                  child: const Padding(
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  child:  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Contraseña",
+                      ),
+                    )
+                   )
+                  ),
+                ),
+                //
+                const SizedBox(height: 20),
+                //BOTON
+                InkWell(
+                  onTap: () {
+                        Navigator.pushNamed(context, ROUTE_MENU);
+                      },
+                  child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20.0
+                  ),
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                    color: Colors.blueAccent,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: const Center(
+                    child: Text("Iniciar Sesion",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                    ),
+                  ),
+                  ),
+                  ),
+                ),
+                //
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("¿No tienes cuenta?",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    InkWell(
+                      onTap: () {
+                        //Navigator.pushNamed(context, ROUTE_REGISTRO);
+                      },
+                      child: const Text("Registrate",
+                       style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.blueAccent
+                      ),),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
+        ),
+      ),
+    ));
   }
 }
