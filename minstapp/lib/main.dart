4a17b186a8f1010f7744d29a145bf791f5255629
AzureAD\MarkEdward <mespinozar@lucky.com.pe>
2025-02-13 15:11:38 -0500

    bloquearOrientacion

--- minstapp/lib/main.dart
+++ minstapp/lib/main.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:minstapp/Common/go_router.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 
 void main() {
-  runApp(const MyApp());
+  // 🔹 Bloquea la orientación en vertical antes de ejecutar la app
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,  // Solo permite modo vertical normal
+    DeviceOrientation.portraitDown, // (Opcional) Permitir también invertido
+  ]).then((_) {
+    runApp(const MyApp());
+  });
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    //SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
     return MaterialApp.router(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
@@ -33,4 +41,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
